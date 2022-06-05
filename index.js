73e42dd899cd1e6a07b728ffd231e3d4a29f3745
Johan <joese6@gmail.com>
2022-06-05 07:40:01 +0700

    feat: challenge-callback

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data,fnData)=>{
+  let sortData=fnData(data);
+  let arrNama=[]
+  for(let i=0;i<sortData.length;i++){
+      arrNama.push(i+1+'. ' +sortData[i]);
+  }
+  return arrNama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data)=>{
+  data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data)=>{
+  data.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
